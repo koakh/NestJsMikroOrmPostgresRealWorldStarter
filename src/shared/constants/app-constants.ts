85e446ea234128ca783e98dc4d67a548c6e11f9f
Mário Monteiro <marioammonteiro@gmail.com>
2023-01-11 12:28:05 +0000

    last push, with code from c3-customers, after this commit we work exclusively in c3-customers

--- src/shared/constants/app-constants.ts
+++ src/shared/constants/app-constants.ts
@@ -2,9 +2,9 @@ import { AppConstants } from '../../shared/interfaces';
 
 export const appConstants: AppConstants = {
   // BOF sync with package.json
-  packageName: 'c3-customers-backend',
+  packageName: 'nestjs-mikro-orm-postgres-real-world-starter',
   packageVersion: '1.0.0',
-  packageDescription: 'NestJS + MikroORM realworld API example (MySQL)',
+  packageDescription: 'NestJS + MikroORM realworld API example (Postgres)',
   // EOF sync with package.json,
   localDomain: '0.0.0.0',
 }
